36e97c08376db1cf41f228a98a48a3a203569407
Dana <data.verdene@stud.hslu.ch>
2023-10-26 12:50:39 +0200

    idk

--- src/tourism_loader.py
+++ src/tourism_loader.py
@@ -1,30 +1,58 @@
 import requests
-import os
 
-def tourism_loader():
-    api_key = os.getenv("TOURISM_API_KEY")
-
-    url = "https://opendata.myswitzerland.io/v1/destinations"
-
-    # Define the headers
+def fetch_and_transform_data(api_key, api_url):
     headers = {
         "accept": "application/json",
-        "x-api-key": f"{api_key}"
+        "x-api-key": api_key
     }
 
-    # Make the GET request with headers
-    response = requests.get(url, headers=headers)
+    destination_list = []
 
-    # Check if the request was successful (status code 200)
-    if response.status_code == 200:
-        # Parse the JSON response
-        data = response.json()
-        # Get all destination titles from the response data
-        destinations = [item["name"] for item in data["data"]]
-        print(type(destinations))
-        print(destinations)
-        
-    else:
-        print("Failed to retrieve data from the API")
+    for page in range(20): #sometimes if fails due to the large number here, might try smaller (e.g. 5), or just rerun
+        url = f"{api_url}&page={page}"
+        response = requests.get(url, headers=headers)
 
-    return(destinations)
+        if response.status_code == 200:
+            data = response.json()
+
+            for entry in data['data']:
+                name = entry.get('name', None)
+                geo = entry.get('geo', {})
+                latitude = geo.get('latitude', None)
+                longitude = geo.get('longitude', None)
+                classifications = entry.get('classification', [])
+
+                classification_list = []
+
+                for classification in classifications:
+                    classification_name = classification.get('name', None)
+                    classification_values = classification.get('values', [])
+
+                    for value in classification_values:
+                        value_name = value.get('name', None)
+                        value_title = value.get('title', None)
+
+                        classification_list.append({
+                            "classification_name": classification_name,
+                            "value_name": value_name,
+                            "value_title": value_title
+                        })
+
+                destination_dict = {
+                    "name": name,
+                    "latitude": latitude,
+                    "longitude": longitude,
+                    "classifications": classification_list
+                }
+
+                destination_list.append(destination_dict)
+        else:
+            print("Failed to retrieve data")
+
+    return destination_list
+
+api_key = 'HFjqHMLW8y8HdSOUjKBfL9JZYPQo5Rvo3Qq1yk3z'
+API_URL_DEST = 'https://opendata.myswitzerland.io/v1/destinations/?hitsPerPage=50&striphtml=true'
+data_dest = fetch_and_transform_data(api_key, API_URL_DEST)
+
+print(data_dest[0]) #to test how the destination looks 
